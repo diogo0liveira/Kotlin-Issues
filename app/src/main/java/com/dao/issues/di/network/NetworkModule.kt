ae9445f19aedd1794c1371de022958858c757c14
Diogo Oliveira <diogo0liveira@hotmail.com>
2019-04-18 09:34:58 -0300

    Adicionado ao repositório.

--- app/src/main/java/com/dao/issues/di/network/NetworkModule.kt
+++ app/src/main/java/com/dao/issues/di/network/NetworkModule.kt
@@ -17,7 +17,6 @@ import okhttp3.logging.HttpLoggingInterceptor.Level
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 /**
